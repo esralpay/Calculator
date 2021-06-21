11584bab2bdec10f09d50c8e84f508ffc01e5b96
esra <esraeraslan@gmail.com>
2021-06-21 11:44:26 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+	
+</script>
+
+<main>
+	<div class="main">
+		<div class="header"></div>
+		<div class="display"></div>
+		<div class="inputs"></div>
+	</div>
+	
+</main>
+
+<style>
+	.main {
+		display: flex;
+		flex-direction: column;		
+		align-items: center;
+		width: 1440px;
+		height: 900px;	
+		background:#3A4663;
+	}
+	.header {
+		background: #242D44;
+		margin-top: 96px;
+		width: 540px;
+		height:44px;
+	}
+	.display {
+		background: #181F33;
+		margin-top: 32px;
+		border-radius: 10px;
+		width: 540px;
+		height: 128px;
+	}
+	.inputs {
+		background: #242D44;
+		margin-top: 24px;
+		border-radius: 10px;
+		width: 540px;
+		height: 480px;
+	}
+
+
+
+</style>
